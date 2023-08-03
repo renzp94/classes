fa22a2daf833c3234944b156cc0596ee11eb0bc6
renzp94 <1074720760@qq.com>
2023-08-03 22:59:17 +0800

    chore: 调整打包为esm格式

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
 	//   打包格式
-	format: ['cjs'],
+	format: ['esm'],
 	// 代码拆分
 	splitting: false,
 	//   打包前清空
 	clean: true,
-	sourcemap: true,
+	sourcemap: false,
 	// 生成d.ts
 	dts: './src/index.ts',
 	//   入口文件
